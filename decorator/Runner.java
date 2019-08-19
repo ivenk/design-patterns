343c4d7871dbbcf2555f7127bb605fe54aa6491c
iven_k <koethke@th-brandenburg.de>
2019-08-19 19:17:26 +0200

    Modified runner to run example

--- decorator/Runner.java
+++ decorator/Runner.java
@@ -1,2 +1,7 @@
 public class Runner {
+
+    public static void main(String args[]) {
+        IDrinkable coffee = new MilkDecorator(new SugarDecorator(new Coffee()));
+        System.out.println(coffee.makeCoffee());
+    }
 }
