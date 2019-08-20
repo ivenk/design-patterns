f3a85ac912443e28444fa32ef73fe734252cbe18
iven_k <koethke@th-brandenburg.de>
2019-08-20 02:24:55 +0200

    Formating

--- decorator/Runner.java
+++ decorator/Runner.java
@@ -3,7 +3,5 @@ public class Runner {
     public static void main(String args[]) {
         IDrinkable coffee = new MilkDecorator(new SugarDecorator(new Coffee()));
         System.out.println(coffee.makeCoffee());
-
-
     }
 }
